140537e4cf131678164553bf15e237d03bf107f0
Fabi <fabi@example.com>
2026-06-09 10:16:39 -0300

    fix: resolve nextauth and mongodb connection issues

--- src/auth.ts
+++ src/auth.ts
@@ -6,6 +6,7 @@ import clientPromise from "@/lib/mongoClient";
 import bcrypt from "bcryptjs";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  trustHost: true,
   adapter: MongoDBAdapter(clientPromise),
   providers: [
     Google({
